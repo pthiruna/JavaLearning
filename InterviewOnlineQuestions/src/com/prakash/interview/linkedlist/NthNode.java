c07366955237e53f47504b7966115124cbb4b477
prakasht <prakasht@PRAKASHT-PC.hq.netapp.com>
2015-02-19 22:42:23 -0500

    initial

---
+++ InterviewOnlineQuestions/src/com/prakash/interview/linkedlist/NthNode.java
@@ -0,0 +1,13 @@
+package com.prakash.interview.linkedlist;
+
+public class NthNode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
